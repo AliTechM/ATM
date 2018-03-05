4fd1c832f475c848b26a5cfa6df922fd599a97c9
John Doe <ali85.tech@gmail.com>
2018-03-05 21:15:36 +0200

    using function

--- ATM.py
+++ ATM.py
@@ -1,22 +1,40 @@
+def withdraw(balance, request):
+    if   request > balance:
+        print("Can't give you all this money !!")
 
-def ATMex(request ,total):
+    elif request < 0:
+        print("More than zero plz!")
 
-    while request>0 and request <total:
-        if request >= 100:
-            print "give 100"
-            request = request -100
-        elif request >=50:
-            print "give 50"
-            request = request -50
-        elif request >=10:
-            print "give 10"
-            request = request -10
-        elif request >=5:
-            print "give 5"
-            request = request -5
-        else:
-            print "give 2"
-            request = request -2
-    return
+    else:
+        balance =balance - request
+        while request > 0:
 
-ATMex(277,500)
+            if request >= 100:
+                request -= 100
+                print("give 100")
+
+            elif request >= 50:
+                request -= 50
+                print("give 50")
+
+            elif request >= 10:
+                request -= 10
+                print("give 10")
+
+            elif request >= 5:
+                request -= 5
+                print("give 5")
+
+            elif request < 5:
+                print("give " + str(request))
+            request = 0
+        print  "Current balance ="+str(balance)  
+    return balance 
+
+balance = 500
+
+
+balance = withdraw(balance, 277)
+balance = withdraw(balance, 30)
+balance = withdraw(balance, 5)
+balance = withdraw(balance, 500)
